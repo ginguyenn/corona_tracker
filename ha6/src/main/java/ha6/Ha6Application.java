b2352ee70f71d65fdb8a03e40775c8ba3bb13b48
ginguyenn <ginguyenn@gmail.com>
2024-01-11 01:07:42 +0100

    First commit

---
+++ ha6/src/main/java/ha6/Ha6Application.java
@@ -0,0 +1,13 @@
+package ha6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ha6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ha6Application.class, args);
+	}
+
+}
---
+++ ha6/src/test/java/ha6/Ha6ApplicationTests.java
@@ -0,0 +1,13 @@
+package ha6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ha6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
