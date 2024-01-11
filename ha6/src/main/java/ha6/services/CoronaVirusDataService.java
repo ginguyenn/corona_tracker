27b0cad26a17384fc22193760e8f7daa5e97573f
ginguyenn <ginguyenn@gmail.com>
2024-01-11 02:38:04 +0100

    add model

--- ha6/src/main/java/ha6/services/CoronaVirusDataService.java
+++ ha6/src/main/java/ha6/services/CoronaVirusDataService.java
@@ -1,8 +1,13 @@
 package ha6.services;
 
 import jakarta.annotation.PostConstruct;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -13,12 +18,21 @@ public class CoronaVirusDataService {
     private static String VIRUS_DATA_URL="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
     @PostConstruct
-    public void fetchVirusData(){
+    @Scheduled(cron = "* * * * * *")
+    public void fetchVirusData() throws IOException, InterruptedException {
         HttpClient client= HttpClient.newHttpClient();
         HttpRequest request= HttpRequest.newBuilder()
                 .uri(URI.create(VIRUS_DATA_URL))
                 .build();
         HttpResponse<String> httpResponse = client.send(request,HttpResponse.BodyHandlers.ofString());
-        System.out.println(HttpResponse.body());
+
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
+        Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(csvBodyReader);
+        for (CSVRecord record : records) {
+            String state = record.get("Province/State");
+            System.out.println(state);
+
+        }
+
     }
 }
